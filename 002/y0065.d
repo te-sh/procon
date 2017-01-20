2f1d3de45fddfd245affa1dd4df042d01f73a511
te-sh <admire.japan@gmail.com>
2017-01-20 20:49:25 +0900

    add No.65

---
+++ 002/y0065.d
@@ -0,0 +1,18 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+version(unittest) {} else
+void main()
+{
+  auto k = readln.chomp.to!size_t;
+
+  auto ei = new real[](k);
+  ei[] = 0;
+
+  foreach_reverse (i; k.iota) {
+    foreach (j; 1..7)
+      if (i + j < k) ei[i] += ei[i + j] / 6;
+    ei[i] += 1;
+  }
+
+  writefln("%.3f", ei[0]);
+}
