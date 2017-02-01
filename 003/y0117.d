f22aaae3ccf1ada7624fe8079db5d07105cd9e64
te-sh <admire.japan@gmail.com>
2017-02-01 11:40:42 +0900

    refactoring No.117

--- 003/y0117.d
+++ 003/y0117.d
@@ -2,29 +2,21 @@ import std.algorithm, std.conv, std.range, std.stdio, std.string;
 import std.regex;     // RegEx
 
 const int p = 10 ^^ 9 + 7;
+alias FactorRing!p mint;
 
 version(unittest) {} else
 void main()
 {
-  auto bufFrac = new int[](2_000_000), sf = 1;
+  auto bufFrac = new mint[](2_000_000), sf = 1;
   bufFrac[0] = 1;
-
-  auto bufInvFrac = new int[](2_000_000);
-  bufInvFrac[] = -1;
 
   auto frac(int n) {
     if (sf <= n) {
       foreach (i; sf..n+1)
-        bufFrac[i] = modMul(bufFrac[i - 1], i, p);
+        bufFrac[i] = bufFrac[i - 1] * i;
       sf = n + 1;
     }
     return bufFrac[n];
-  }
-
-  auto invFrac(int n) {
-    if (bufInvFrac[n] == -1)
-      bufInvFrac[n] = invMod(frac(n), p);
-    return bufInvFrac[n];
   }
 
   auto t = readln.chomp.to!size_t;
@@ -33,29 +25,21 @@ void main()
     auto m = rd.matchFirst(r"([CPH])\((\d+),(\d+)\)");
     auto n = m[2].to!int, k = m[3].to!int;
 
-    auto r = 0;
+    auto r = mint(0);
     switch (m[1]) {
     case "C":
-      if (n >= k) {
-        r = frac(n);
-        r = modMul(r, invFrac(k), p);
-        r = modMul(r, invFrac(n - k), p);
-      }
+      if (n >= k)
+        r = frac(n) * frac(k).inv * frac(n - k).inv;
       break;
     case "P":
-      if (n >= k) {
-        r = frac(n);
-        r = modMul(r, invFrac(n - k), p);
-      }
+      if (n >= k)
+        r = frac(n) * frac(n - k).inv;
       break;
     case "H":
-      if (n > 0) {
-        r = frac(n + k - 1);
-        r = modMul(r, invFrac(k), p);
-        r = modMul(r, invFrac(n - 1), p);
-      } else if (k == 0) {
+      if (n > 0)
+        r = frac(n + k - 1) * frac(k).inv * frac(n - 1).inv;
+      else if (k == 0)
         r = 1;
-      }
       break;
     default:
       assert(0);
@@ -65,9 +49,47 @@ void main()
   }
 }
 
-int modMul(int a, int b, int mod)
-{
-  return ((a.to!long * b.to!long) % mod).to!int;
+struct FactorRing(int m) {
+  long v;
+
+  @property int toInt() { return v.to!int; }
+  alias toInt this;
+
+  this(T)(T _v) { v = mod(_v); }
+
+  ref FactorRing!m opAssign(int _v) {
+    v = mod(_v);
+    return this;
+  }
+
+  auto mod(long _v) {
+    if (_v > 0) return _v % m;
+    else return ((_v % m) + m) % m;
+  }
+
+  auto opBinary(string op: "+")(FactorRing!m rhs) { return FactorRing!m(v + rhs.v); }
+  auto opBinary(string op: "-")(FactorRing!m rhs) { return FactorRing!m(v - rhs.v); }
+  auto opBinary(string op: "*")(FactorRing!m rhs) { return FactorRing!m(v * rhs.v); }
+  auto opBinary(string op: "^^")(FactorRing!m rhs) { return pow(this, rhs.toInt); }
+
+  auto opBinary(string op)(int rhs)
+    if (op == "+" || op == "-" || op == "*") { return opBinary!op(FactorRing!m(rhs)); }
+  auto opBinary(string op: "^^")(int rhs) { return pow(this, rhs); }
+
+  auto pow(FactorRing!m a, int b) {
+    if (b == 0) return FactorRing!m(1);
+    if (a == 0) return FactorRing!m(0);
+    auto c = FactorRing!m(1);
+    for (; b > 0; a = a * a, b >>= 1)
+      if ((b & 1) == 1) c = c * a;
+    return c;
+  }
+
+  auto inv() {
+    int x = v.to!int, a, b;
+    exEuclid(x, m, a, b);
+    return FactorRing!m(a);
+  }
 }
 
 T exEuclid(T)(T a, T b, ref T x, ref T y)
@@ -80,11 +102,4 @@ T exEuclid(T)(T a, T b, ref T x, ref T y)
     y -= a / b * x;
   }
   return g;
-}
-
-T invMod(T)(T x, T m)
-{
-  T a, b;
-  exEuclid(x, m, a, b);
-  return ((a % m) + m) % m;
 }
