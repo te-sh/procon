cb8d831ad4ff5cad2982b0de5cd813bce72b184a
te-sh <admire.japan@gmail.com>
2017-02-14 17:52:18 +0900

    add No.183

---
+++ 004/y0183.d
@@ -0,0 +1,19 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+version(unittest) {} else
+void main()
+{
+  auto n = readln.chomp.to!size_t;
+  auto ai = readln.split.to!(int[]);
+
+  auto dp = new bool[][](n + 1, 1 << 15);
+  dp[0][1] = true;
+  foreach (i, a; ai)
+    foreach (j; (1 << 15).iota)
+      if (dp[i][j]) {
+        dp[i + 1][j] = true;
+        dp[i + 1][j ^ a] = true;
+      }
+
+  writeln(dp[n].count!(a => a));
+}
