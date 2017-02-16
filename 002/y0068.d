d050e43411f349ea6c544dc8e354a7ccb4365301
te-sh <admire.japan@gmail.com>
2017-02-16 18:07:02 +0900

    add No.68

---
+++ 002/y0068.d
@@ -0,0 +1,24 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+import std.container; // SList, DList, BinaryHeap
+
+version(unittest) {} else
+void main()
+{
+  auto n = readln.chomp.to!size_t;
+  auto li = readln.split.to!(real[]);
+  auto q = readln.chomp.to!size_t;
+  auto ki = readln.split.to!(size_t[]);
+
+  struct LSP { real l, s; int p; }
+
+  auto bi = li.map!(l => LSP(l, l, 1)).array.heapify!"a.s < b.s";
+  auto ri = new real[](ki.fold!max + 1);
+  foreach (i; 1..ri.length) {
+    auto b = bi.front;
+    ri[i] = b.s;
+    bi.replaceFront(LSP(b.l, b.l / (b.p + 1), b.p + 1));
+  }
+
+  foreach (k; ki)
+    writefln("%.10f", ri[k]);
+}
