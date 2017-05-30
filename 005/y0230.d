7271987ded3dc52d6f8d0f0607b2940d65ac0ab2
te-sh <admire.japan@gmail.com>
2017-05-30 10:55:15 +0900

    use librarized Segment Tree on No.230

--- 005/y0230.d
+++ 005/y0230.d
@@ -1,12 +1,14 @@
 import std.algorithm, std.conv, std.range, std.stdio, std.string;
 
+alias SegmentTreeLazy!(int, 0, "a + b") SegTree;
+
 version(unittest) {} else
 void main()
 {
   auto n = readln.chomp.to!size_t;
   auto q = readln.chomp.to!size_t;
 
-  auto sa = SegmentTree(n), sb = SegmentTree(n);
+  auto sa = SegTree(n), sb = SegTree(n);
   auto pa = 0L, pb = 0L;
 
   foreach (_; 0..q) {
@@ -18,12 +20,12 @@ void main()
       if (ca < cb) pb += cb;
       break;
     case "1":
-      sa[l..r] = true;
-      sb[l..r] = false;
+      sa[l..r] = 1;
+      sb[l..r] = 0;
       break;
     case "2":
-      sa[l..r] = false;
-      sb[l..r] = true;
+      sa[l..r] = 0;
+      sb[l..r] = 1;
       break;
     default:
       assert(0);
@@ -36,70 +38,86 @@ void main()
   writeln(pa, " ", pb);
 }
 
-struct SegmentTree
+struct SegmentTreeLazy(T, T unit, alias pred = "a + b")
 {
-  import core.bitop, std.conv;
+  import core.bitop, std.conv, std.functional, std.range;
+  alias predFun = binaryFun!pred;
+  enum Op { none, fill, add };
 
   const size_t n, an;
-  int[] buf;
-  bool[] laz, prop;
+  T[] buf, laz;
+  Op[] op;
 
   this(size_t n)
   {
     this.n = n;
     an = (1 << ((n - 1).bsr + 1));
-    buf = new int[](an * 2);
-    laz = new bool[](an * 2);
-    prop = new bool[](an * 2);
+    buf = new T[](an * 2);
+    laz = new T[](an * 2);
+    op = new Op[](an * 2);
+    static if (T.init != unit) {
+      buf[] = unit;
+    }
   }
 
   void propagate(size_t k, size_t nl, size_t nr)
   {
-    if (!prop[k]) return;
+    if (op[k] == Op.none) return;
 
     size_t nm = (nl + nr) / 2;
-    setLazy(laz[k], k*2,   nl, nm);
-    setLazy(laz[k], k*2+1, nm, nr);
+    setLazy(op[k], laz[k], k*2,   nl, nm);
+    setLazy(op[k], laz[k], k*2+1, nm, nr);
 
-    prop[k] = false;
+    op[k] = Op.none;
   }
 
-  void setLazy(bool val, size_t k, size_t nl, size_t nr)
+  void setLazy(Op nop, T val, size_t k, size_t nl, size_t nr)
   {
-    buf[k] = val ? (nr - nl).to!int : 0;
-    laz[k] = val;
-    prop[k] = true;
+    switch (nop) {
+    case Op.fill:
+      buf[k] = val * (nr - nl).to!T;
+      laz[k] = val;
+      op[k] = nop;
+      break;
+    case Op.add:
+      buf[k] += val * (nr - nl).to!T;
+      laz[k] = op[k] == Op.none ? val : laz[k] + val;
+      op[k] = op[k] == Op.fill ? Op.fill : Op.add;
+      break;
+    default:
+      assert(0);
+    }
   }
 
-  void opSliceAssign(bool val, size_t l, size_t r)
-  {
-    fill(val, l, r, 1, 0, an);
-  }
-
-  void fill(bool val, size_t l, size_t r, size_t k, size_t nl, size_t nr)
+  void addOpe(Op op, T val, size_t l, size_t r, size_t k, size_t nl, size_t nr)
   {
     if (nr <= l || r <= nl) return;
 
     if (l <= nl && nr <= r) {
-      setLazy(val, k, nl, nr);
+      setLazy(op, val, k, nl, nr);
       return;
     }
     
     propagate(k, nl, nr);
 
     auto nm = (nl + nr) / 2;
-    fill(val, l, r, k*2,   nl, nm);
-    fill(val, l, r, k*2+1, nm, nr);
+    addOpe(op, val, l, r, k*2,   nl, nm);
+    addOpe(op, val, l, r, k*2+1, nm, nr);
 
-    buf[k] = buf[k*2] + buf[k*2+1];
+    buf[k] = predFun(buf[k*2], buf[k*2+1]);
   }
 
-  int opSlice(size_t l, size_t r)
+  void opSliceAssign(T val, size_t l, size_t r)
   {
-    return count(l, r, 1, 0, an);
+    addOpe(Op.fill, val, l, r, 1, 0, an);
   }
 
-  int count(size_t l, size_t r, size_t k, size_t nl, size_t nr)
+  void opSliceOpAssign(string op: "+")(T val, size_t l, size_t r)
+  {
+    addOpe(Op.add, val, l, r, 1, 0, an);
+  }
+
+  int summary(size_t l, size_t r, size_t k, size_t nl, size_t nr)
   {
     if (nr <= l || r <= nl) return 0;
 
@@ -108,10 +126,15 @@ struct SegmentTree
     propagate(k, nl, nr);
 
     auto nm = (nl + nr) / 2;
-    auto vl = count(l, r, k*2,   nl, nm);
-    auto vr = count(l, r, k*2+1, nm, nr);
+    auto vl = summary(l, r, k*2,   nl, nm);
+    auto vr = summary(l, r, k*2+1, nm, nr);
 
-    return vl + vr;
+    return predFun(vl, vr);
+  }
+
+  int opSlice(size_t l, size_t r)
+  {
+    return summary(l, r, 1, 0, an);
   }
 
   pure size_t opDollar() const { return n; }
