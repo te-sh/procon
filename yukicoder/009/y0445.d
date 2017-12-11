d9bf37eb584ed412e83da29eea3b5237aaee7832
te-sh <admire.japan@gmail.com>
2017-12-11 10:15:53 +0900

    add yukicoder No.445

---
+++ yukicoder/009/y0445.d
@@ -0,0 +1,8 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+version(unittest) {} else
+void main()
+{
+  auto rd = readln.split.to!(int[]), a = rd[0], b = rd[1];
+  writeln(50*a+(500*a)/(8+2*b));
+}
