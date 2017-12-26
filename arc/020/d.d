2f128b794f51c5d5656ea0b9f9d33da9ed13abd7
te-sh <admire.japan@gmail.com>
2017-12-26 17:49:00 +0900

    add ARC #020 D

---
+++ arc/020/d.d
@@ -0,0 +1,110 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+const mod = 10^^9+7;
+alias mint = FactorRing!mod;
+
+version(unittest) {} else
+void main()
+{
+  auto rd = readln.split.to!(int[]), n = rd[0], m = rd[1], k = rd[2];
+  auto d = new int[](n);
+  foreach (i; 0..n-1) d[i] = readln.chomp.to!int;
+
+  auto f = new int[](1<<k);
+  foreach (i; 0..1<<k)
+    foreach (j; 0..k-1)
+      if (i.bitTest(j) ^ i.bitTest(j+1)) ++f[i];
+
+  auto dp = new mint[][][](n+1, 1<<k, m);
+  dp[0][0][0] = 1;
+
+  foreach (i; 0..n)
+    foreach (j; 0..1<<k)
+      foreach (u; 0..m) {
+        dp[i+1][j][(u+d[i]*f[j]) % m] += dp[i][j][u];
+        foreach (v; 0..k) {
+          if (j.bitTest(v)) continue;
+          auto j2 = j.bitSet(v);
+          auto u2 = (u + d[i]*f[j2]) % m;
+          dp[i+1][j2][u2] += dp[i][j][u];
+        }
+      }
+
+  writeln(dp[n][(1<<k)-1][0]);
+}
+
+pragma(inline) {
+  pure bool bitTest(T)(T n, size_t i) { return (n & (T(1) << i)) != 0; }
+  pure T bitSet(T)(T n, size_t i) { return n | (T(1) << i); }
+  pure T bitReset(T)(T n, size_t i) { return n & ~(T(1) << i); }
+  pure T bitComp(T)(T n, size_t i) { return n ^ (T(1) << i); }
+
+  pure T bitSet(T)(T n, size_t s, size_t e) { return n | ((T(1) << e) - 1) & ~((T(1) << s) - 1); }
+  pure T bitReset(T)(T n, size_t s, size_t e) { return n & (~((T(1) << e) - 1) | ((T(1) << s) - 1)); }
+  pure T bitComp(T)(T n, size_t s, size_t e) { return n ^ ((T(1) << e) - 1) & ~((T(1) << s) - 1); }
+
+  import core.bitop;
+  pure int bsf(T)(T n) { return core.bitop.bsf(ulong(n)); }
+  pure int bsr(T)(T n) { return core.bitop.bsr(ulong(n)); }
+  pure int popcnt(T)(T n) { return core.bitop.popcnt(ulong(n)); }
+}
+
+struct FactorRing(int m, bool pos = false)
+{
+  version(BigEndian) {
+    union { long vl; struct { int vi2; int vi; } }
+  } else {
+    union { long vl; int vi; }
+  }
+
+  static init() { return FactorRing!(m, pos)(0); }
+
+  @property int toInt() { return vi; }
+  alias toInt this;
+
+  this(int v) { vi = v; }
+  this(int v, bool runMod) { vi = runMod ? mod(v) : v; }
+  this(long v) { vi = mod(v); }
+
+  ref FactorRing!(m, pos) opAssign(int v) { vi = v; return this; }
+
+  pure auto mod(int v) const
+  {
+    static if (pos) return v % m;
+    else return (v % m + m) % m;
+  }
+
+  pure auto mod(long v) const
+  {
+    static if (pos) return cast(int)(v % m);
+    else return cast(int)((v % m + m) % m);
+  }
+
+  static if (!pos) {
+    pure auto opUnary(string op: "-")() const { return FactorRing!(m, pos)(mod(-vi)); }
+  }
+
+  static if (m < int.max / 2) {
+    pure auto opBinary(string op: "+")(int rhs) const { return FactorRing!(m, pos)(mod(vi + rhs)); }
+    pure auto opBinary(string op: "-")(int rhs) const { return FactorRing!(m, pos)(mod(vi - rhs)); }
+  } else {
+    pure auto opBinary(string op: "+")(int rhs) const { return FactorRing!(m, pos)(mod(vl + rhs)); }
+    pure auto opBinary(string op: "-")(int rhs) const { return FactorRing!(m, pos)(mod(vl - rhs)); }
+  }
+  pure auto opBinary(string op: "*")(int rhs) const { return FactorRing!(m, pos)(mod(vl * rhs)); }
+
+  pure auto opBinary(string op)(FactorRing!(m, pos) rhs) const
+    if (op == "+" || op == "-" || op == "*") { return opBinary!op(rhs.vi); }
+
+  static if (m < int.max / 2) {
+    auto opOpAssign(string op: "+")(int rhs) { vi = mod(vi + rhs); }
+    auto opOpAssign(string op: "-")(int rhs) { vi = mod(vi - rhs); }
+  } else {
+    auto opOpAssign(string op: "+")(int rhs) { vi = mod(vl + rhs); }
+    auto opOpAssign(string op: "-")(int rhs) { vi = mod(vl - rhs); }
+  }
+  auto opOpAssign(string op: "*")(int rhs) { vi = mod(vl * rhs); }
+
+  auto opOpAssign(string op)(FactorRing!(m, pos) rhs)
+    if (op == "+" || op == "-" || op == "*") { return opOpAssign!op(rhs.vi); }
+}
