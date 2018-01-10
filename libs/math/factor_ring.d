4d5cbb5db73332b697013dcd5b7df9414b414883
te-sh <admire.japan@gmail.com>
2018-01-10 16:01:25 +0900

    refactor point and factor_ring

--- libs/math/factor_ring.d
+++ libs/math/factor_ring.d
@@ -1,11 +1,6 @@
 struct FactorRing(int m, bool pos = false)
 {
-  version(BigEndian) {
-    union { long vl; struct { int vi2; int vi; } }
-  } else {
-    union { long vl; int vi; }
-  }
-
+  version(BigEndian) union { long vl; struct { int vi2; int vi; } } else union { long vl; int vi; }
   static init() { return FactorRing!(m, pos)(0); }
 
   @property int toInt() { return vi; }
@@ -17,50 +12,28 @@ struct FactorRing(int m, bool pos = false)
 
   ref FactorRing!(m, pos) opAssign(int v) { vi = v; return this; }
 
-  pure auto mod(int v) const
-  {
-    static if (pos) return v % m;
-    else return (v % m + m) % m;
-  }
+  pure auto mod(int v) const { static if (pos) return v%m; else return (v%m+m)%m; }
+  pure auto mod(long v) const { static if (pos) return cast(int)(v%m); else return cast(int)((v%m+m)%m); }
 
-  pure auto mod(long v) const
-  {
-    static if (pos) return cast(int)(v % m);
-    else return cast(int)((v % m + m) % m);
-  }
-
-  static if (!pos) {
-    pure auto opUnary(string op: "-")() const { return FactorRing!(m, pos)(mod(-vi)); }
-  }
+  static if (!pos) pure auto opUnary(string op: "-")() { return FactorRing!(m, pos)(mod(-vi)); }
 
   static if (m < int.max / 2) {
-    pure auto opBinary(string op: "+")(int rhs) const { return FactorRing!(m, pos)(mod(vi + rhs)); }
-    pure auto opBinary(string op: "-")(int rhs) const { return FactorRing!(m, pos)(mod(vi - rhs)); }
+    pure auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FactorRing!(m, pos)(mod(mixin("vi"~op~"r"))); }
+    auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vi"~op~"r")); return this; }
   } else {
-    pure auto opBinary(string op: "+")(int rhs) const { return FactorRing!(m, pos)(mod(vl + rhs)); }
-    pure auto opBinary(string op: "-")(int rhs) const { return FactorRing!(m, pos)(mod(vl - rhs)); }
+    pure auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FactorRing!(m, pos)(mod(mixin("vl"~op~"r"))); }
+    auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vl"~op~"r")); return this; }
   }
-  pure auto opBinary(string op: "*")(int rhs) const { return FactorRing!(m, pos)(mod(vl * rhs)); }
+  pure auto opBinary(string op: "*")(int r) const { return FactorRing!(m, pos)(mod(vl*r)); }
+  auto opOpAssign(string op: "*")(int r) { vi = mod(vl*r); return this; }
 
-  pure auto opBinary(string op)(FactorRing!(m, pos) rhs) const
-    if (op == "+" || op == "-" || op == "*") { return opBinary!op(rhs.vi); }
+  pure auto opBinary(string op)(FactorRing!(m, pos) r) if (op == "+" || op == "-" || op == "*") { return opBinary!op(r.vi); }
+  auto opOpAssign(string op)(FactorRing!(m, pos) r) if (op == "+" || op == "-" || op == "*") { return opOpAssign!op(r.vi); }
 
-  static if (m < int.max / 2) {
-    auto opOpAssign(string op: "+")(int rhs) { vi = mod(vi + rhs); }
-    auto opOpAssign(string op: "-")(int rhs) { vi = mod(vi - rhs); }
-  } else {
-    auto opOpAssign(string op: "+")(int rhs) { vi = mod(vl + rhs); }
-    auto opOpAssign(string op: "-")(int rhs) { vi = mod(vl - rhs); }
-  }
-  auto opOpAssign(string op: "*")(int rhs) { vi = mod(vl * rhs); }
-
-  auto opOpAssign(string op)(FactorRing!(m, pos) rhs)
-    if (op == "+" || op == "-" || op == "*") { return opOpAssign!op(rhs.vi); }
-
-  pure auto opBinary(string op: "/")(FactorRing!(m, pos) rhs) { return FactorRing!(m, pos)(mod(vl * rhs.inv.vi)); }
-  pure auto opBinary(string op: "/")(int rhs) { return opBinary!op(FactorRing!(m, pos)(rhs)); }
-  auto opOpAssign(string op: "/")(FactorRing!(m, pos) rhs) { vi = mod(vl * rhs.inv.vi); }
-  auto opOpAssign(string op: "/")(int rhs) { return opOpAssign!op(FactorRing!(m, pos)(rhs)); }
+  pure auto opBinary(string op: "/")(FactorRing!(m, pos) r) { return FactorRing!(m, pos)(mod(vl * r.inv.vi)); }
+  pure auto opBinary(string op: "/")(int r) { return opBinary!op(FactorRing!(m, pos)(r)); }
+  auto opOpAssign(string op: "/")(FactorRing!(m, pos) r) { vi = mod(vl * r.inv.vi); return this; }
+  auto opOpAssign(string op: "/")(int r) { return opOpAssign!op(FactorRing!(m, pos)(r)); }
 
   pure auto inv() const
   {
--- libs/point.d
+++ libs/point.d
@@ -1,28 +1,28 @@
 struct Point(T)
 {
   T x, y;
-  pure auto opBinary(string op: "+")(Point!T rhs) const { return Point!T(x + rhs.x, y + rhs.y); }
-  pure auto opBinary(string op: "-")(Point!T rhs) const { return Point!T(x - rhs.x, y - rhs.y); }
-  pure auto opBinary(string op: "*")(Point!T rhs) const { return x * rhs.x + y * rhs.y; }
-  pure auto opBinary(string op: "*")(T a) const { return Point!T(x * a, y * a); }
-  pure auto opBinary(string op: "/")(T a) const { return Point!T(x / a, y / a); }
-  pure auto hypot2() const { return x ^^ 2 + y ^^ 2; }
+  pure auto opBinary(string op)(Point!T r) if (op == "+" || op == "-") { return mixin("Point!T(x"~op~"r.x,y"~op~"r.y)"); }
+  pure auto opBinary(string op)(T a) if (op == "*" || op == "/") { return mixin("Point!T(x"~op~"a,y"~op~"a)"); }
+  auto opOpAssign(string op)(Point!T r) if (op == "+" || op == "-") { mixin("x"~op~"=r.x; y"~op~"=r.y;"); return this; }
+  auto opOpAssign(string op)(T a) if (op == "*" || op == "/") { mixin("x"~op~"=a; y"~op~"=a;"); return this; }
+  pure auto opBinary(string op: "*")(Point!T r) { return x*r.x + y*r.y; }
+  pure auto hypot2() const { return x^^2 + y^^2; }
 }
 
 struct Point3(T)
 {
   T x, y, z;
-  pure auto opBinary(string op: "+")(Point3!T rhs) const { return Point3!T(x + rhs.x, y + rhs.y, z + rhs.z); }
-  pure auto opBinary(string op: "-")(Point3!T rhs) const { return Point3!T(x - rhs.x, y - rhs.y, z - rhs.z); }
-  pure auto opBinary(string op: "*")(Point3!T rhs) const { return x * rhs.x + y * rhs.y + z * rhs.z; }
-  pure auto opBinary(string op: "*")(T a) const { return Point3!T(x * a, y * a, z * a); }
-  pure auto opBinary(string op: "/")(T a) const { return Point3!T(x / a, y / a, z / a); }
-  pure auto hypot2() const { return x ^^ 2 + y ^^ 2 + z ^^ 2; }
+  pure auto opBinary(string op)(Point3!T r) if (op == "+" || op == "-") { return mixin("Point3!T(x"~op~"r.x,y"~op~"r.y,z"~op~"r.z)"); }
+  pure auto opBinary(string op)(T a) if (op == "*" || op == "/") { return mixin("Point3!T(x"~op~"a,y"~op~"a,z"~op~"a)"); }
+  auto opOpAssign(string op)(Point3!T r) if (op == "+" || op == "-") { mixin("x"~op~"=r.x; y"~op~"=r.y; z"~op~"=r.z;"); return this; }
+  auto opOpAssign(string op)(T a) if (op == "*" || op == "/") { mixin("x"~op~"=a; y"~op~"=a; z"~op~"=a;"); return this; }
+  pure auto opBinary(string op: "*")(Point3!T r) { return x*r.x + y*r.y + z*r.z; }
+  pure auto hypot2() const { return x^^2 + y^^2 + z^^2; }
 }
 
 pure auto outerProd(T)(Point3!T p1, Point3!T p2)
 {
-  return Point3!T(p1.y * p2.z - p1.z * p2.y, p1.z * p2.x - p1.x * p2.z, p1.x * p2.y - p1.y * p2.x);
+  return Point3!T(p1.y*p2.z - p1.z*p2.y, p1.z*p2.x - p1.x*p2.z, p1.x*p2.y - p1.y*p2.x);
 }
 
 unittest
@@ -42,6 +42,15 @@ unittest
   assert(p1 / 3 == point(0, 1));
 
   assert(p1.hypot2 == 20);
+
+  p1 += p2;
+  assert(p1 == point(3, 7));
+  p1 -= p2;
+  assert(p1 == point(2, 4));
+  p1 *= 2;
+  assert(p1 == point(4, 8));
+  p1 /= 2;
+  assert(p1 == point(2, 4));
 }
 
 unittest
@@ -64,4 +73,13 @@ unittest
   assert(p1.hypot2 == 45);
 
   assert(outerProd(p1, p2) == point3(-7, 1, 2));
+
+  p1 += p2;
+  assert(p1 == point3(3, 7, 7));
+  p1 -= p2;
+  assert(p1 == point3(2, 4, 5));
+  p1 *= 2;
+  assert(p1 == point3(4, 8, 10));
+  p1 /= 2;
+  assert(p1 == point3(2, 4, 5));
 }
