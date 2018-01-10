10dd5ec81cf822179c9571a8b761e4ab8e77ebb4
te-sh <admire.japan@gmail.com>
2018-01-10 10:24:03 +0900

    fix yukicoder No.372

--- yukicoder/008/y0372.d
+++ yukicoder/008/y0372.d
@@ -5,20 +5,21 @@ const mod = 10 ^^ 9 + 7;
 version(unittest) {} else
 void main()
 {
-  auto s = readln.chomp.map!(a => (a - '0').to!int).array, n = s.length;
+  auto s = readln.chomp;
   auto m = readln.chomp.to!int;
 
-  auto dp1 = new int[][](2, m);
+  auto dp1 = new int[][](2, m), dp2 = new int[][](2, m);
   dp1[0][0] = 1;
 
-  foreach (p; 0..n) {
-    auto dp2 = dp1.map!(a => a.dup).array;
+  foreach (c; s) {
+    auto d = cast(int)(c-'0');
+    foreach (q; 0..2) dp2[q][] = dp1[q][];
     foreach (q; 0..2)
       foreach (r; 0..m)
-        if (s[p] > 0 || q == 1)
-          (dp2[1][(r * 10 + s[p]) % m] += dp1[q][r]) %= mod;
-    dp1 = dp2;
+        if (d > 0 || q == 1)
+          (dp2[1][(r * 10 + d) % m] += dp1[q][r]) %= mod;
+    swap(dp1, dp2);
   }
 
-  writeln(dp1[1][0] + s.count(0));
+  writeln(dp1[1][0] + s.count('0'));
 }
